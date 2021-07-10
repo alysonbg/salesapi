d5983c97ea82c05853567f0d98549e27349d3744
Alyson Brito <alysongirao@gmail.com>
2021-07-09 21:41:42 -0300

    User model

--- salesapiproject/settings.py
+++ salesapiproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',
     'rest_framework.authtoken',
+    'salesapiproject.users',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,5 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'salesapiproject.users.User'
