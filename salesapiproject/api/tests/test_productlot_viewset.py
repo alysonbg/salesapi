31d079748f07037bd72f19b14e23bdc8fb5e990c
Alyson Brito <alysongirao@gmail.com>
2021-07-11 14:59:17 -0300

    Refactored viewset and tests for auth

--- salesapiproject/api/tests/test_productlot_viewset.py
+++ salesapiproject/api/tests/test_productlot_viewset.py
@@ -3,8 +3,8 @@ from django.shortcuts import reverse as r
 
 
 @pytest.fixture
-def get_response(db, client, product_lot):
-    return client.get(r('productlot-list'))
+def get_response(db, auth_client, product_lot):
+    return auth_client.get(r('productlot-list'))
 
 
 def test_get_response_status_code(get_response):
@@ -15,13 +15,18 @@ def test_get_response_body(get_response):
     assert 1 == len(get_response.data)
 
 
+def test_get_unauthorize(db, client):
+    response = client.get(r('productlot-list'))
+    assert 401 == response.status_code
+
+
 @pytest.fixture
-def post_response(db, client):
+def post_response(db, auth_client):
     data = {
         'code': 'testeproductlot123',
         'manufacturing_date': '2021-07-11'
     }
-    return client.post(r('productlot-list'), data=data)
+    return auth_client.post(r('productlot-list'), data=data)
 
 
 def test_post_response_status_code(post_response):
@@ -38,13 +43,28 @@ def test_post_response_body(post_response):
     assert data == post_response.data
 
 
+def test_post_response_unauthorized(db, client):
+    data = {
+        'code': 'testeproductlot123',
+        'manufacturing_date': '2021-07-11'
+    }
+    response = client.post(r('productlot-list'), data=data)
+    assert 401 == response.status_code
+
+
 @pytest.fixture
-def delete_response(db, client, product_lot):
-    return client.delete(r('productlot-detail', kwargs={'pk': product_lot.pk}))
+def delete_response(db, auth_client, product_lot):
+    return auth_client.delete(r('productlot-detail', kwargs={'pk': product_lot.pk}))
 
 
 def test_delete_response_status_code(delete_response):
     assert 204 == delete_response.status_code
+
+
+def delete_unauthorized(db, client, product_lot):
+    response = client.delete(r('productlot-detail', kwargs={'pk': product_lot.pk}))
+
+    assert 401 == response.status_code
 
 
 @pytest.fixture
@@ -55,6 +75,13 @@ def patch_response(db, auth_client, product_lot):
 
 def test_patch_response_status_code(patch_response):
     assert 200 == patch_response.status_code
+
+
+def test_patch_unauthorized(db, client, product_lot):
+    data = {'manufacturing_date': '2021-07-19'}
+    response = client.patch(r('productlot-detail', kwargs={'pk': product_lot.pk}), data=data)
+
+    assert 401 == response.status_code
 
 
 def test_put_not_allowed(db, auth_client, product_lot):
