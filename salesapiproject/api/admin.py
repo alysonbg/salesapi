b8d1ffed9f2d96e1820de33ed45be987a7a94561
Alyson Brito <alysongirao@gmail.com>
2021-07-11 22:48:34 -0300

    Refactored OrderSerializer

--- salesapiproject/api/admin.py
+++ salesapiproject/api/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 
-from salesapiproject.api.models import ProductLot, Product
+from salesapiproject.api.models import ProductLot, Product, Order, OrderLine, Client
 
 admin.site.register(ProductLot)
 admin.site.register(Product)
+admin.site.register(Order)
+admin.site.register(OrderLine)
+admin.site.register(Client)
